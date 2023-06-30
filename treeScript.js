6f2cb1b67342d21b4f1cdc59c9ddd83faa7b44c1
shogoisaji <simacsimac9@gmail.com>
2023-06-30 18:55:28 +0900

    changeTreeScript

--- treeScript.js
+++ treeScript.js
@@ -10,20 +10,22 @@ var baseSphereRadius = 6;  // 基本半径
 var angularSpeed = 0;      // 角速度
 
 // 玉の配置パラメータ
-var numLevels = 15;        // 段数
+var numLevels = 20;        // 段数
 var numSpheresPerLevel = 20; // 段ごとの玉の数
-var spheresSpan = 28;      // 段ごとの距離
+var spheresSpan = 21;      // 段ごとの距離
 var distanceLevel = 3.0;     // 玉の前後レベル
 
 // view設定(楕円率)
-var viewAngleLevel = 6;
-
-// 段ごとの玉の軌道半径
-var levelOrbitRadiusX = [65, 55, 55, 60, 160, 180, 190, 185, 170, 152, 155, 140, 120, 90, 40];
+var viewAngleLevel = 5;
 
 // フレームレート調整用
 var frameCount = 0;
 
+// 一部の段の玉数を調整
+var addSpheresPerLevel = 4;
+
+// 段ごとの玉の軌道半径
+var levelOrbitRadiusX = [75, 65, 62, 60, 65, 130, 170, 180, 185, 183, 180, 170, 158, 160, 162, 150, 135, 110, 80, 45];
 
 // 玉クラス
 class Sphere {
@@ -52,25 +54,29 @@ class Sphere {
   }
 
   move(angularSpeed) {
-    var angle = angularSpeed + this.index * 2 * Math.PI / (this.numSpheresPerLevel);
+    var angle = angularSpeed + this.index * 2 * Math.PI / this.numSpheresPerLevel;
 
-    this.element.setAttribute('cx', this.centerX + this.radiusX * Math.cos(angle));
+    this.element.setAttribute('cx', this.centerX + this.radiusX * Math.cos(angle+= this.level*2));
     this.element.setAttribute('cy', this.centerY + this.radiusX / this.viewAngleLevel * Math.sin(angle) - this.level * this.spheresSpan);
 
     var nowRadius = this.baseRadius + Math.sin(angle) * this.distanceLevel;
     this.element.setAttribute('r', nowRadius);
 
     var saturation = Math.abs(Math.cos(angle/2-0.8)) * 100;
-    this.element.style.fill = 'hsl(120, ' + saturation + '%, 40%)';
+    this.element.style.fill = 'hsl(120, ' + saturation + '%, ' + (this.level*1.8+20) +'%)';
+
+    if(Math.cos(angle) > 0.2) {
+      this.element.style.display = 'none';
+    } else {
+      this.element.style.display = '';
+    }
   }
 }
 
-var addSpheresPerLevel = 0;
-
 var spheres = [];
 for (var level = 0; level < numLevels; level++) {
-  if(level < 4 || level > 13){
-    addSpheresPerLevel = 8;
+  if(level < 5 || level > 18){
+    if(level == numLevels-1){addSpheresPerLevel += 5;}
   }else addSpheresPerLevel = 0;
   for (var i = 0; i < numSpheresPerLevel; i++) {
     var newSphere = new Sphere(level, i, centerX, centerY, levelOrbitRadiusX, numSpheresPerLevel-addSpheresPerLevel, baseSphereRadius, distanceLevel, viewAngleLevel, spheresSpan);
@@ -81,12 +87,12 @@ for (var level = 0; level < numLevels; level++) {
 
 function moveSpheres() {
   // フレームカウンタが偶数のときだけ更新を行う
-  if (frameCount % 3 === 0) {
+  if (frameCount % 2 === 0) {
     for (var i = 0; i < spheres.length; i++) {
       spheres[i].move(angularSpeed);
     }
 
-    angularSpeed += 0.012;
+    angularSpeed += 0.01;
   }
 
   frameCount++;
@@ -94,4 +100,3 @@ function moveSpheres() {
 }
 
 moveSpheres();
-
